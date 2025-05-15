43b0d61c6860094b5df324a5fd52fae3c60ff8cc
yakovliam <jacob@yakovliam.com>
2025-05-14 21:33:56 -0400

    refactor: refactors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import Layout from "./components/layout/Layout";
+import Layout from "@/components/layout/Layout";
 
-import CribsPage from "./pages/cribs/CribsPage";
-import IndividualCrib from "./pages/individualCrib/IndividualCrib";
-import ProfilePage from "./pages/profile/ProfilePage";
+import CribsPage from "@/pages/cribs/CribsPage";
+import IndividualCrib from "@/pages/individualCrib/IndividualCrib";
+import ProfilePage from "@/pages/profile/ProfilePage";
 
 function App() {
   return (
