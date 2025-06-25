192d8a5453171d0467d080b3efb2eeb351d97f45
Chris Farah <farahca@mail.uc.edu>
2025-06-24 22:16:46 -0400

    feat: individual page fin

--- src/pages/cribs/individual/IndividualPage.tsx
+++ src/pages/cribs/individual/IndividualPage.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeftIcon } from "lucide-react";
 import IndividualSlider from "./IndividualSlider";
 import { useNavigate } from "react-router";
+import { useGetPublicCribPostid } from "@/gen";
 const initialState = {
   user: {
     id: 1,
@@ -28,6 +29,13 @@ const initialState = {
 const IndividualPage = () => {
   const navigate = useNavigate();
   //fetch images from server and pass them to the slider prop
+  const id = window.location.pathname.split("/").pop() || "";
+  console.log("ID:", id);
+  const {
+    data: post,
+    error: post_error,
+    isLoading: post_isLoading,
+  } = useGetPublicCribPostid(id);
   const images = [
     "https://picsum.photos/id/100/600/600",
     "https://picsum.photos/id/101/600/600",
@@ -46,59 +54,78 @@ const IndividualPage = () => {
       <div>
         <IndividualSlider images={images} />
       </div>
-      <div className="flex ">
-        <div>
-          <img
-            alt="profile"
-            src="https://picsum.photos/id/103/600/600"
-            className="rounded-full h-20 m-5 shadow-2xl border"
-          />
+      {post_isLoading && (
+        <div className="flex items-center justify-center w-full">
+          <span>Loading...</span>
         </div>
-        <div className="flex flex-col justify-center border-b">
-          <div className="font-semibold text-xl">{initialState.user.name} </div>
-          <div>@{initialState.user.username}</div>
+      )}
+      {post_error && (
+        <div className="flex items-center justify-center w-full ">
+          <span>Error loading post</span>
         </div>
-      </div>
-      <div>
-        <div className="text-3xl font-semibold p-4">{initialState.title}</div>
-        <div className="text-lg px-6">{initialState.description}</div>
-        <div className="text-lg px-8 py-3 flex">
-          <div className="font-semibold flex mr-1">Price: </div>{" "}
-          {initialState.price}
-        </div>
-        <div className="text-lg pb-3 px-8 flex">
-          <div className="font-semibold mr-1">Roommates:</div>
-          {initialState.roommates}
-        </div>
-        <div className="text-lg px-8 flex mb-3">
-          <div className="mr-1 font-semibold">Lease from:</div>{" "}
-          {new Date(initialState.BeginDate).toLocaleDateString()} to{" "}
-          {new Date(initialState.EndDate).toLocaleDateString()}
-        </div>
-        <div className="text-lg px-8 flex">
-          <div className="mr-1 font-semibold">Tags:</div>
-        </div>
-        <div className="flex justify-center border rounded-xl mx-8 my-2 py-2">
-          <div className="flex max-w-[400px] flex-wrap p-4 justify-center">
-            {initialState.tags.map((item, index) => (
-              <div
-                key={index}
-                className="flex p-2 bg-black text-white m-1 rounded-full shadow-xl"
-              >
-                {item}
+      )}
+      {post && (
+        <>
+          <div className="flex ">
+            <div>
+              <img
+                alt="profile"
+                src="https://picsum.photos/id/103/600/600"
+                className="rounded-full h-20 m-5 shadow-2xl border"
+              />
+            </div>
+            <div className="flex flex-col justify-center border-b">
+              <div className="font-semibold text-xl">
+                {post.data.firstName + " " + post.data.lastName}{" "}
               </div>
-            ))}
+              <div>@{post.data.username}</div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="flex flex-row-reverse px-8 py-2">
-        <button
-          className="bg-blue-500 rounded-full p-3 px-4 shadow-xl text-white underline cursor-pointer "
-          onClick={() => navigate(`/profile/${initialState.user.username}`)}
-        >
-          Contact
-        </button>
-      </div>
+          <div>
+            <div className="text-3xl font-semibold p-4">{post.data.title}</div>
+            <div className="text-lg px-6">{post.data.description}</div>
+            <div className="text-lg px-8 py-3 flex">
+              <div className="font-semibold flex mr-1">Price: </div>{" "}
+              {post.data.price}
+            </div>
+            <div className="text-lg pb-3 px-8 flex">
+              <div className="font-semibold mr-1">Roommates:</div>
+              {post.data.roommates}
+            </div>
+            <div className="text-lg px-8 flex mb-3">
+              <div className="mr-1 font-semibold">Lease from:</div>{" "}
+              {post.data.termStartDate &&
+                new Date(post.data.termStartDate).toLocaleDateString()}{" "}
+              to{" "}
+              {post.data.termEndDate &&
+                new Date(post.data.termEndDate).toLocaleDateString()}
+            </div>
+            <div className="text-lg px-8 flex">
+              <div className="mr-1 font-semibold">Tags:</div>
+            </div>
+            <div className="flex justify-center border rounded-xl mx-8 my-2 py-2">
+              <div className="flex max-w-[400px] flex-wrap p-4 justify-center">
+                {post.data.tags?.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex p-2 bg-black text-white m-1 rounded-full shadow-xl"
+                  >
+                    {item.name}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse px-8 py-2">
+            <button
+              className="bg-blue-500 rounded-full p-3 px-4 shadow-xl text-white underline cursor-pointer "
+              onClick={() => navigate(`/profile/${post.data.username}`)}
+            >
+              Contact
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
